8202ff55ddfbc9fe1812afeec1159add46215722
Alexis Brard <alexis.brard@insa-rennes.fr>
2017-03-04 17:00:11 +0100

    Added the structure Pipe

--- game/src/game.h
+++ game/src/game.h
@@ -7,13 +7,27 @@
 
 /*!
 * \struct Bird game.h
+* \brief The bird that the player controls by taping on the screen or clicking on the mouse
 */
 typedef struct{
-    int x;              /*!< the abscissa coordonate */
-    int y;              /*!< the ordonate coordonate */
-    char sprite;        /*!< the path for the sprite */
+    int x;              /*!< the bird abscissa coordonate */
+    int y;              /*!< the bird ordonate coordonate */
+    char sprite;        /*!< the path for the bird sprite */
     int gravity;        /*!< the speed of climb/fall of the bird */
 } Bird;
+
+/*!
+* \struct Pipe game.h
+* \brief The pipes that the bird has to avoid
+*/
+typedef struct{
+    int x;              /*!< the pipe abscissa coordonate */
+    int y;              /*!< the pipe ordonate coordonate */
+    char sprite;        /*!< the path for the pipe sprite */
+    int speed;          /*!< the speed of scrolling of the pipes */
+}
+
+
 
 
 #endif // GAME_H
