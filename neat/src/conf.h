c5b0b9ee3927215b50211b1f6b0c0e6e0e8b2e41
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-03-07 15:18:43 +0100

    Added config file for neural network

---
+++ neat/src/conf.h
@@ -0,0 +1,13 @@
+/*!
+* \file conf.h
+* \brief File containing neural network paramaters
+*/
+#ifndef CONF_H
+#define CONF_H
+
+#define POPULATION    200
+
+#define INPUTS        3
+#define OUTPUTS       1
+
+#endif // CONF_H
