34dacd104bcedc49ba01116b71347345936c2504
Le Chenadec Roman <rlechena@insa-rennes.fr>
2017-03-09 14:20:09 +0100

    Created a function that detect if the bird hit something or not

--- game/src/game.c
+++ game/src/game.c
@@ -14,7 +14,22 @@ void cameraScrolling(Camera * camera, Bird * bird)
     camera->x += camera->speed;
     bird->x += camera->speed;
 }
-
-
+/*!
+* \brief Detect the collisions
+* \param[in] bird the bird to test the collision with
+* \param[in] obstacle the only obstacle that the bird can hit
+* \return 1 if the bird hit something 0 in other cases
+*/
+int detectHit(Bird * bird, Obstacle * obstacle)
+{
+    int h = 0;
+    if(bird->y+bird->h/2 >= SCREEN_HEIGHT) h = 1; //detect the collision with the ground
+    if(bird->x + bird->w/2 >= obstacle->lower->x)
+    {
+        if(bird->y - bird->h/2 <= obstacle->upper->y + obstacle->upper->h) h = 1;
+        if(bird->y + bird->h/2 >= obstacle->lower->y) h = 1;
+    }
+    return h;
+}
 
 
