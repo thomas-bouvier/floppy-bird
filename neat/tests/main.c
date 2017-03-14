35fa49503fff3276e4581228a8c863ce656ad8c8
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-03-14 16:57:42 +0100

    Added unit tests for empty and setOnFirst functions

--- neat/tests/main.c
+++ neat/tests/main.c
@@ -56,6 +56,27 @@ static void test_emptyNeuronList(void ** state) {
   assert_int_equal(emptyNeuronList(neuron_list), 1);
 }
 
+static int setup_emptyNeuronListOneElementRemoved(void ** state) {
+  NeuronList * neuron_list = newNeuronList();
+
+  if (neuron_list == NULL)
+    return -1;
+
+  initNeuronList(neuron_list);
+  addNeuron(neuron_list, newNeuron(UNKNOW));
+  deleteNeuron(neuron_list, getCurrentNeuron(neuron_list));
+
+  *state = neuron_list;
+
+  return 0;
+}
+
+static void test_emptyNeuronListOneElementRemoved(void ** state) {
+  NeuronList * neuron_list = (NeuronList *) (* state);
+
+  assert_int_equal(emptyNeuronList(neuron_list), 1);
+}
+
 static int teardown_emptyNeuronList(void ** state) {
   freeNeuronList(*state);
   return 0;
@@ -80,11 +101,6 @@ static void test_outOfNeuronList(void ** state) {
   NeuronList * neuron_list = (NeuronList *) (* state);
 
   assert_int_equal(outOfNeuronList(neuron_list), 1);
-}
-
-static int teardown_outOfNeuronList(void ** state) {
-  freeNeuronList(*state);
-  return 0;
 }
 
 static int setup_outOfNeuronListOneElement(void ** state) {
@@ -126,6 +142,86 @@ static void test_outOfNeuronListOneElementNext(void ** state) {
   NeuronList * neuron_list = (NeuronList *) (* state);
 
   assert_int_equal(outOfNeuronList(neuron_list), 1);
+}
+
+static int teardown_outOfNeuronList(void ** state) {
+  freeNeuronList(*state);
+  return 0;
+}
+
+/* setOnFirstNeuron */
+
+static int setup_setOnFirstNeuronEmptyList(void ** state) {
+  NeuronList * neuron_list = newNeuronList();
+
+  if (neuron_list == NULL)
+    return -1;
+
+  initNeuronList(neuron_list);
+
+  *state = neuron_list;
+
+  return 0;
+}
+
+static void test_setOnFirstNeuronEmptyList(void ** state) {
+  NeuronList * neuron_list = (NeuronList *) (* state);
+
+  setOnFirstNeuron(neuron_list);
+
+  assert_null(neuron_list->current);
+}
+
+static int setup_setOnFirstNeuronOneElement(void ** state) {
+  NeuronList * neuron_list = newNeuronList();
+
+  if (neuron_list == NULL)
+    return -1;
+
+  initNeuronList(neuron_list);
+  addNeuron(neuron_list, newNeuron(UNKNOW));
+
+  *state = neuron_list;
+
+  return 0;
+}
+
+static void test_setOnFirstNeuronOneElement(void ** state) {
+  NeuronList * neuron_list = (NeuronList *) (* state);
+
+  setOnFirstNeuron(neuron_list);
+
+  assert_ptr_equal(neuron_list->current, neuron_list->first);
+}
+
+static int setup_setOnFirstNeuron(void ** state) {
+  NeuronList * neuron_list = newNeuronList();
+
+  if (neuron_list == NULL)
+    return -1;
+
+  initNeuronList(neuron_list);
+  addNeuron(neuron_list, newNeuron(UNKNOW));
+  addNeuron(neuron_list, newNeuron(BIAS));
+  addNeuron(neuron_list, newNeuron(INPUT));
+  addNeuron(neuron_list, newNeuron(OUTPUT));
+
+  *state = neuron_list;
+
+  return 0;
+}
+
+static void test_setOnFirstNeuron(void ** state) {
+  NeuronList * neuron_list = (NeuronList *) (* state);
+
+  setOnFirstNeuron(neuron_list);
+
+  assert_ptr_equal(neuron_list->current, neuron_list->first);
+}
+
+static int teardown_setOnFirstNeuron(void ** state) {
+  freeNeuronList(*state);
+  return 0;
 }
 
 /* initConnectionGeneList */
@@ -171,6 +267,27 @@ static int setup_emptyConnectionGeneList(void ** state) {
 }
 
 static void test_emptyConnectionGeneList(void ** state) {
+  ConnectionGeneList * connection_gene_list = (ConnectionGeneList *) (* state);
+
+  assert_int_equal(emptyConnectionGeneList(connection_gene_list), 1);
+}
+
+static int setup_emptyConnectionGeneListOneElementRemoved(void ** state) {
+  ConnectionGeneList * connection_gene_list = newConnectionGeneList();
+
+  if (connection_gene_list == NULL)
+    return -1;
+
+  initConnectionGeneList(connection_gene_list);
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+  deleteConnectionGene(connection_gene_list, getCurrentConnectionGene(connection_gene_list));
+
+  *state = connection_gene_list;
+
+  return 0;
+}
+
+static void test_emptyConnectionGeneListOneElementRemoved(void ** state) {
   ConnectionGeneList * connection_gene_list = (ConnectionGeneList *) (* state);
 
   assert_int_equal(emptyConnectionGeneList(connection_gene_list), 1);
@@ -248,19 +365,102 @@ static void test_outOfConnectionGeneListOneElementNext(void ** state) {
   assert_int_equal(outOfConnectionGeneList(connection_gene_list), 1);
 }
 
+/* setOnFirstNeuron */
+
+static int setup_setOnFirstConnectionGeneEmptyList(void ** state) {
+  ConnectionGeneList * connection_gene_list = newConnectionGeneList();
+
+  if (connection_gene_list == NULL)
+    return -1;
+
+  initConnectionGeneList(connection_gene_list);
+
+  *state = connection_gene_list;
+
+  return 0;
+}
+
+static void test_setOnFirstConnectionGeneEmptyList(void ** state) {
+  ConnectionGeneList * connection_gene_list = (ConnectionGeneList *) (* state);
+
+  setOnFirstConnectionGene(connection_gene_list);
+
+  assert_null(connection_gene_list->current);
+}
+
+static int setup_setOnFirstConnectionGeneOneElement(void ** state) {
+  ConnectionGeneList * connection_gene_list = newConnectionGeneList();
+
+  if (connection_gene_list == NULL)
+    return -1;
+
+  initConnectionGeneList(connection_gene_list);
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+
+  *state = connection_gene_list;
+
+  return 0;
+}
+
+static void test_setOnFirstConnectionGeneOneElement(void ** state) {
+  ConnectionGeneList * connection_gene_list = (ConnectionGeneList *) (* state);
+
+  setOnFirstConnectionGene(connection_gene_list);
+
+  assert_ptr_equal(connection_gene_list->current, connection_gene_list->first);
+}
+
+static int setup_setOnFirstConnectionGene(void ** state) {
+  ConnectionGeneList * connection_gene_list = newConnectionGeneList();
+
+  if (connection_gene_list == NULL)
+    return -1;
+
+  initConnectionGeneList(connection_gene_list);
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+  addConnectionGene(connection_gene_list, newConnectionGene(0.0, 0, 1));
+
+  *state = connection_gene_list;
+
+  return 0;
+}
+
+static void test_setOnFirstConnectionGene(void ** state) {
+  ConnectionGeneList * connection_gene_list = (ConnectionGeneList *) (* state);
+
+  setOnFirstConnectionGene(connection_gene_list);
+
+  assert_ptr_equal(connection_gene_list->current, connection_gene_list->first);
+}
+
+static int teardown_setOnFirstConnectionGene(void ** state) {
+  freeConnectionGeneList(*state);
+  return 0;
+}
+
 int main() {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test_setup_teardown(test_initNeuronList, setup_initNeuronList, teardown_initNeuronList),
     cmocka_unit_test_setup_teardown(test_emptyNeuronList, setup_emptyNeuronList, teardown_emptyNeuronList),
+    cmocka_unit_test_setup_teardown(test_emptyNeuronListOneElementRemoved, setup_emptyNeuronListOneElementRemoved, teardown_emptyNeuronList),
     cmocka_unit_test_setup_teardown(test_outOfNeuronList, setup_outOfNeuronList, teardown_outOfNeuronList),
     cmocka_unit_test_setup_teardown(test_outOfNeuronListOneElement, setup_outOfNeuronListOneElement, teardown_outOfNeuronList),
     cmocka_unit_test_setup_teardown(test_outOfNeuronListOneElementNext, setup_outOfNeuronListOneElementNext, teardown_outOfNeuronList),
+    cmocka_unit_test_setup_teardown(test_setOnFirstNeuronEmptyList, setup_setOnFirstNeuronEmptyList, teardown_setOnFirstNeuron),
+    cmocka_unit_test_setup_teardown(test_setOnFirstNeuronOneElement, setup_setOnFirstNeuronOneElement, teardown_setOnFirstNeuron),
+    cmocka_unit_test_setup_teardown(test_setOnFirstNeuron, setup_setOnFirstNeuron, teardown_setOnFirstNeuron),
 
     cmocka_unit_test_setup_teardown(test_initConnectionGeneList, setup_initConnectionGeneList, teardown_initConnectionGeneList),
     cmocka_unit_test_setup_teardown(test_emptyConnectionGeneList, setup_emptyConnectionGeneList, teardown_emptyConnectionGeneList),
+    cmocka_unit_test_setup_teardown(test_emptyConnectionGeneListOneElementRemoved, setup_emptyConnectionGeneListOneElementRemoved, teardown_emptyConnectionGeneList),
     cmocka_unit_test_setup_teardown(test_outOfConnectionGeneList, setup_outOfConnectionGeneList, teardown_outOfConnectionGeneList),
     cmocka_unit_test_setup_teardown(test_outOfConnectionGeneListOneElement, setup_outOfConnectionGeneListOneElement, teardown_outOfConnectionGeneList),
     cmocka_unit_test_setup_teardown(test_outOfConnectionGeneListOneElementNext, setup_outOfConnectionGeneListOneElementNext, teardown_outOfConnectionGeneList),
+    cmocka_unit_test_setup_teardown(test_setOnFirstConnectionGeneEmptyList, setup_setOnFirstConnectionGeneEmptyList, teardown_setOnFirstConnectionGene),
+    cmocka_unit_test_setup_teardown(test_setOnFirstConnectionGeneOneElement, setup_setOnFirstConnectionGeneOneElement, teardown_setOnFirstConnectionGene),
+    cmocka_unit_test_setup_teardown(test_setOnFirstConnectionGene, setup_setOnFirstConnectionGene, teardown_setOnFirstConnectionGene),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
