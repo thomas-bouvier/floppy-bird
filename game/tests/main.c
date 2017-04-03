17da5117e1bc5fb7a7722df2e1d2ee8953533343
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-04-03 19:01:44 +0200

    Added unit tests for Bird

--- game/tests/main.c
+++ game/tests/main.c
@@ -14,9 +14,8 @@
 static int setup_initBird(void ** state) {
   Bird * bird = malloc(sizeof(Bird));
 
-  if (bird == NULL) {
+  if (bird == (Bird *) NULL)
     return -1;
-  }
 
   *state = bird;
 
@@ -35,6 +34,70 @@ static void test_initBird(void ** state) {
 }
 
 static int teardown_initBird(void ** state) {
+  free(*state);
+  return 0;
+}
+
+/* updateBird */
+
+static int setup_updateBird(void ** state) {
+  Bird * bird = malloc(sizeof(Bird));
+
+  if (bird == (Bird *) NULL)
+    return -1;
+
+  *state = bird;
+  initBird(*state);
+
+  return 0;
+}
+
+static void test_updateBirdNothing(void ** state) {
+  Bird * bird = (Bird *) (* state);
+
+  int y = bird->y;
+  int dir_y = bird->dir_y;
+
+  updateBird(*state, NOTHING);
+
+  assert_int_equal(bird->dir_y, dir_y + GRAVITY);
+  assert_int_equal(bird->y, y + dir_y + GRAVITY);
+}
+
+static void test_updateBirdNothingMaxFallSpeed(void ** state) {
+  int i;
+  Bird * bird = (Bird *) (* state);
+
+  for (i = 0; i < 100; ++i)
+    updateBird(*state, NOTHING);
+
+  assert_int_equal(bird->dir_y, BIRD_MAX_FALL_SPEED);
+  assert_int_equal(bird->y, SCREEN_HEIGHT - BIRD_SIZE / 2);
+}
+
+static void test_updateBirdJump(void ** state) {
+  Bird * bird = (Bird *) (* state);
+
+  int y = bird->y;
+
+  updateBird(*state, JUMP);
+
+  assert_int_equal(bird->dir_y, BIRD_JUMP);
+  assert_int_equal(bird->y, y + BIRD_JUMP);
+}
+
+static void test_updateBirdJumpMaxHeight(void ** state) {
+  int i;
+  Bird * bird = (Bird *) (* state);
+
+  for (i = 0; i < 100; ++i)
+    updateBird(*state, JUMP);
+
+  assert_int_equal(bird->dir_y, BIRD_JUMP);
+  assert_int_equal(bird->y, BIRD_SIZE / 2);
+}
+
+static int teardown_updateBird(void ** state) {
   free(*state);
   return 0;
 }
@@ -93,6 +156,11 @@ static void test_newObstacleMaxHeight(void ** state) {
 int main() {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test_setup_teardown(test_initBird, setup_initBird, teardown_initBird),
+
+    cmocka_unit_test_setup_teardown(test_updateBirdNothing, setup_updateBird, teardown_updateBird),
+    cmocka_unit_test_setup_teardown(test_updateBirdNothingMaxFallSpeed, setup_updateBird, teardown_updateBird),
+    cmocka_unit_test_setup_teardown(test_updateBirdJump, setup_updateBird, teardown_updateBird),
+    cmocka_unit_test_setup_teardown(test_updateBirdJumpMaxHeight, setup_updateBird, teardown_updateBird),
 
     cmocka_unit_test_setup_teardown(test_newObstacle, setup_newObstacle, teardown_newObstacle),
     cmocka_unit_test_setup_teardown(test_newObstacleNegativeGap, setup_newObstacle, teardown_newObstacle),
