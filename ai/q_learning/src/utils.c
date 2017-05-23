71376f4ae052025443bafefd98a438b81481f378
Théo Chatelier <tchateli@insa-rennes.fr>
2017-05-23 09:49:52 +0200

    Updated functions and comments of the utils.c file

--- ai/q_learning/src/utils.c
+++ ai/q_learning/src/utils.c
@@ -3,39 +3,26 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <limits.h>
 
 /*!
-* \brief Return a random number between 0 and the specified parameter
-* \param[in] limit the upper limit
-* \return Return a random number in the interval [0; limit]
-*/
-int randomAtMost(int limit) {
-  	int r;
-  	int d;
- 
-  	if (limit == 0) return 0;
-
-  	d = RAND_MAX / limit;
-  	limit *= d;
-
-  	do {
-    	r = rand();
-  	} while (r >= limit);
-
-  	return r / d;
-}
-
-/*!
-* \brief Return a random number in a range given
+* \brief Return a random number in a given range
 * \param[in] limit_min the lower limit
 * \param[in] limit_max the upper limit
-* \return Return a random number in the interval [limit_min; limit_max]
+* \return Return a random number in the interval [limit_min; limit_max], 0 and message if error occurred
 */
-int randomInRange(int limit_min, int limit_max) {
-  	int range= 1 + limit_max - limit_min;
-  	int d=RAND_MAX / range;
-    int limit= range*d;
-    int r;
+unsigned int randomInRange(unsigned int limit_min, unsigned int limit_max) {
+  	
+	if((limit_min<0) || (limit_max<limit_min) || (limit_max>UINT_MAX))
+	{
+		printf("Error due to the random function in the qlearning/utils.c file.\n");
+		return 0;
+	}
+
+	unsigned int range= 1 + limit_max - limit_min;
+  	unsigned int d=RAND_MAX / range;
+    unsigned int limit= range*d;
+    unsigned int r;
 
     if (limit_max == 0) return 0;
 
@@ -43,14 +30,14 @@ int randomInRange(int limit_min, int limit_max) {
     	r = rand();
   	} while (r >= limit);
     
-  	return limit_min+ (r/d);
+  	return limit_min+(r/d);
 }
 
 /*!
-* \brief Give the value raised at the power
+* \brief Give the value raised at the given power
 * \param[in] value value to raise at
 * \param[in] power power
-* \return Return the value raised at the power given
+* \return Return the value raised at the given power
 */
 float powerOf(float value, int power)
 {
@@ -65,7 +52,7 @@ float powerOf(float value, int power)
 * \param[in] array array to be shifted
 * \param[in] size size of the array
 */
-void shift_array(int * array, int size)
+void rightShift_array(int * array, int size)
 {
 	int i;
 	for(i=size-1; i>0; --i) array[i]=array[i-1];
@@ -83,22 +70,3 @@ void init_array(int * array, int size, int value)
 	int i;
 	for(i=size-1; i>=0; --i) array[i]=value;
 }
-
-/*!
-* \brief Wait during the given value in ms
-* \param[in] mseconds waiting duration
-*/
-/*void delay(unsigned int mseconds)
-{
-    clock_t goal = mseconds + clock();
-    while (goal > clock());
-}*/
-
-/*!
-* \brief Clear the terminal screen (for debug purpose)
-*/
-/*void clearScreen()
-{
-  const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
-  write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
-}*/
--- ai/q_learning/src/utils.h
+++ ai/q_learning/src/utils.h
@@ -5,12 +5,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-int randomAtMost(int limit);
 int randomInRange(int limit_min, int limit_max);
 float powerOf(float value, int power);
-void shift_array(int * array, int size);
+void rightShift_array(int * array, int size);
 void init_array(int * array, int size, int value);
-/*void delay(unsigned int mseconds);
-void clearScreen();*/
 
 #endif
